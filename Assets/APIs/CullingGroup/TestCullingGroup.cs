62c68af8f5e5f95fa9141db58750175acce81197
woo <engineerwoo@outlook.com>
2022-11-15 11:15:15 +0800

    Culling Group And BoundingShpere

---
+++ Assets/APIs/CullingGroup/TestCullingGroup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCullingGroup : MonoBehaviour
+{
+    public Transform SphereParent;
+    public Camera cam;
+    public BoundingSphere[] allBounds;
+    private CullingGroup cg;
+    private Transform[] allSpheres;
+    private void OnEnable()
+    {
+        allSpheres = SphereParent.GetComponentsInChildren<Transform>();
+        allBounds = new BoundingSphere[allSpheres.Length];
+        for (int i = 0; i < allSpheres.Length; i++)
+        {
+            var tr = allSpheres[i];
+            allBounds[i] = new BoundingSphere(tr.position,tr.localScale.x * 0.5f);
+        }
+        cg = new CullingGroup();
+        cg.targetCamera = cam;
+        cg.SetBoundingSpheres(allBounds);
+        cg.onStateChanged = OnStateChanged; 
+    }
+
+    private void OnStateChanged(CullingGroupEvent cgEvent)
+    {
+        var index = cgEvent.index;
+        var isVisible = cgEvent.isVisible;
+        var curDistance = cgEvent.currentDistance;
+        var preDistance = cgEvent.previousDistance;
+        var wasVisible = cgEvent.wasVisible;
+        var hasBecomeVisible = cgEvent.hasBecomeVisible;
+        Debug.Log($"index{index}  isvisible{isVisible}  curDistance{curDistance}  preDistance{preDistance}  wasVisible{wasVisible}  hasBecomeVisible{hasBecomeVisible}");
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < allSpheres.Length; i++)
+        {
+            var tr = allSpheres[i];
+            allBounds[i].position = tr.position;
+            allBounds[i].radius = tr.localScale.x * 0.5f;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log($"isvisible{cg.IsVisible(1)}  distance{cg.GetDistance(1)}" );
+        }
+    }
+
+    private void OnDisable()
+    {
+        cg.Dispose();
+        cg = null;
+    }
+}
